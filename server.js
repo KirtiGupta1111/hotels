e622ac90f0b5cd79273c2ea92ff8464ffc1c11da
Kirti gupta <kirtigupta111@gmail.com>
2024-06-17 04:00:08 +0530

    first commit

--- server.js
+++ server.js
@@ -1,98 +1,36 @@
-// // console.log("sever file is running..");
+const express = require('express')
+const app = express();
+const db = require('./db');
+require('dotenv').config();
+const passport = require('./auth');
 
-//const { json } = require("express");
-
-// // function add(a,b)
-// // {
-// //   return a+b;
-// // }
-
-// // let result = add(2,3);
-// // console.log(result);
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 
-// // function callBack()
-// // {
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+app.use(logRequest);
 
-// //   console.log("I am Calling a Callback Function");
-// // }
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false})
 
-// // const add = function(a, b, callBack){
-// //   const result = a+b;
-// //   console.log(result);
-// //   callBack();
-// // }
+app.get('/', function (req, res) {
+    res.send('Welcome to our Hotel');
+})
 
-// // add(3,4,callBack);
-// // 
-
-
-
-// const { log } = require("console");
-// let fs = require("fs");
-// let os = require("os");
-
-// let user = os.userInfo();
-// console.log(user);
-// console.log(user.username);
-
-// fs.appendFile(" greeting.txt ",  "  Hi  "  +  user.username  +  '!\n',  () => {
-//   console.log("file is created");
-// });
-// console.log(os);
-
-// const notes = require("./notes.js");
-// const _ = require("lodash");
-// console.log("Sever file available");
-
-// const age = notes.age;
-// console.log(age);
-
-// let data = ["person", "person", 1,2,1,2, "name", "age", "2"];
-// let filter = _.uniq(data);
-
-// console.log(filter);
-
-// console.log(_.isString());
-
-
-
-// const jsonString = '{"name": "kirti", "age" : 23, "city": "gurgaon"}';
-// const jsonToObject = JSON.parse(jsonString);
-// console.log(jsonToObject.age);
-// console.log(jsonToObject.name);
-
-
-// const objToConvert = {
-//   name : "Alice",
-//   age : 24,
-// };
-
-// const json = JSON.stringify(objToConvert);
-// console.log(json);
-// // console.log(typeof json);
-
-
-const express =  require("express");
-const app =  express();
-const db = require("./db");
-
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
- app.get("/", (req, res) =>{
-  res.send("Hello World");
- }) 
-
+// Import the router files
 const personRoutes = require('./routes/personRoutes');
-const MenuItemRoutes = require('./routes/menuItemRoutes');
+const menuItemRoutes = require('./routes/menuItemRoutes');
+
+// Use the routers
 app.use('/person', personRoutes);
-app.use('/menu', MenuItemRoutes);
-
-
-const port = 8001;
-
-app.listen(port , ()=>{
-  console.log(`listening on port ${port}`);
-});
+app.use('/menu', menuItemRoutes);
+  
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
